954e7469464c72cf7f88cf542b63162249c9b904
Caetano Laprebendere <caetanolaprebendere@MacBook-Pro-3.local>
2018-02-05 14:08:38 +0800

    Polished all commands and made songs searchable by name

--- spotify.rb
+++ spotify.rb
@@ -7,15 +7,41 @@ command, *values = ARGV
 playlist = []
 
 if command == "--add"
-	p "Please add your song name and specify wether it is played or unplayed"
     Song.create(song_name: values[0], played_status: values[1])
 	puts "Your song " + values[0] + " has been added."
-elsif command == "--list"
-	p Song.all
-elsif command == "--update" 
-	values.delete
-	values = gets.chomp
-elsif command == "--remove"	
-	values.destroy
+  
+  elsif command == "--list"
+
+	print "
+	No.   Song Name       Played Status
+	===   ===========     =============
+	"  
+
+    i = 1 
+
+	Song.all.order("played_status").each do |song|
+			puts "        #{i}     #{song.song_name}  #{song.played_status}          "
+			i += 1
+	end
+	puts
+  
+  elsif command == "--update" 
+	song = Song.find_by(song_name: values[0])
+    song.update(song_name: values[1], played_status: values [2])
+    puts "you updated your song info"
+  
+  elsif command == "--remove"	
+	song = Song.find_by(song_name: values[0])
+	if song
+	song.destroy
+	puts "you removed the song from your playlist" 
+  end
+
+    for n in 0..Song.all.length-1
+	Song.all.order("played_status").each do |n| 
+	end
+	end
+
 end
+
 
